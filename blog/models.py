cceeffef0bee4739976ebc0fc26ae0f80056ae00
Shilcof <robert.shilcof@gmail.com>
2020-05-21 10:43:34 +0100

    Blog app running

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+    title = models.TextField()
+    publish_date = models.DateField()
+    summary = models.CharField(max_length=1000)
+    image = models.ImageField(upload_to='images/')
