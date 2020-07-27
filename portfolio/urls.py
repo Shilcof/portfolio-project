ed39267ff704e6c678521ff253533056a2875993
Shilcof <robert.shilcof@gmail.com>
2020-07-27 15:10:13 +0100

    404

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -25,3 +25,5 @@ urlpatterns = [
     path('', jobs.views.home, name='home'),
     path('blog/', include('blog.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = 'jobs.views.page_not_found_view'
