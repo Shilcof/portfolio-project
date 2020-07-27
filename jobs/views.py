7380f7738f2b96cc70a0e19dff4fa410964b7fb9
Shilcof <robert.shilcof@gmail.com>
2020-07-27 15:36:04 +0100

    404 edit II

--- jobs/views.py
+++ jobs/views.py
@@ -5,7 +5,3 @@ from .models import Job
 def home(request):
     jobs = Job.objects
     return render(request, 'jobs/home.html', {'jobs':jobs})
-
-def page_not_found_view(request):
-    jobs = Job.objects
-    return render(request, 'jobs/page_not_found_view.html', {'jobs':jobs})
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.http import handler404
 import jobs.views
 
 
@@ -26,5 +25,3 @@ urlpatterns = [
     path('', jobs.views.home, name='home'),
     path('blog/', include('blog.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-handler404 = 'jobs.views.page_not_found_view'
