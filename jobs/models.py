5345fdf89bd7378e014e3fc133e662d4a40f812a
Shilcof <robert.shilcof@gmail.com>
2020-07-30 15:21:11 +0100

    Job extension

--- jobs/models.py
+++ jobs/models.py
@@ -3,4 +3,4 @@ from django.db import models
 class Job(models.Model):
     #upload to images file within uploads
     image = models.ImageField(upload_to='images/')
-    summary = models.CharField(max_length=200)
+    summary = models.CharField(max_length=1000)
